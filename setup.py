acf8eb1a26d1110d95c0954347fd61e372be00de
Craig Weber <crgwbr@gmail.com>
2018-11-28 13:08:27 -0500

    Add Python 3.7 to tests

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     author='Craig Weber',
     author_email='crgwbr@gmail.com',
